8540d867ebbbb8e9233b560b550d7f837048c1d4
GSHCamaro <149434698+GSHCamaro@users.noreply.github.com>
2024-10-19 05:51:15 +0000

    atuador finalizado

--- Atuador.cpp
+++ Atuador.cpp
@@ -1,11 +1,12 @@
-#ifndef ATUADOR_CPP
-#define ATUADOR_CPP
+#ifndef ATUADOR_CPP // Verifica se ATUADOR_CPP não foi definido ainda
+#define ATUADOR_CPP // Define ATUADOR_CPP
+// Elas garantem que o conteúdo entre #ifndef e #endif seja incluído apenas uma vez durante a compilação
 
-#include <iostream>     // Inclusão da biblioteca para input/output
+#include <iostream>     // Inclusão da biblioteca iostream
 #include "Componente.cpp"  // Inclusão do arquivo Componente.cpp, que contem a classe Componente
 #include "Sensor.cpp"      // Inclusão do arquivo Sensor.cpp, que contem a classe Sensor
 
-// Classe base Atuador, que herda da classe Componente
+// Classe Atuador, que herda da classe Componente
 class Atuador : public Componente {
 public:
     // Construtor da classe Atuador que recebe o nome e passa para o construtor da classe base (Componente)
@@ -28,27 +29,27 @@ public:
     Ventilador() : Atuador("Ventilador"), velocidade(0) {}
 
     // Função para definir a velocidade com base na temperatura
-    bool setVelocidade(int temperatura) { // Temperatura ideal é 22°C
+    bool setVelocidade(int temperatura) { // Temperatura ideal é entre 24°C a 22°C
         // Ajusta a velocidade com base na temperatura
         if (temperatura == 30) {
             velocidade = 1000;  // Temperatura muito alta, velocidade máxima
         } else if (temperatura == 29) {
-            velocidade = 800;   // Temperatura um pouco mais baixa
+            velocidade = 800;   // Temperatura um alta, velocidade alta
         } else if (temperatura == 28 || temperatura == 27) {
-            velocidade = 400;   // Temperatura média
+            velocidade = 400;   // Temperatura média, velocidade alta 
         } else if (temperatura == 26 || temperatura == 25) {
-            velocidade = 200;   // Temperatura um pouco mais baixa
+            velocidade = 200;   // Temperatura baixa, velocidade baixa
         } else {
-            velocidade = 0;     // Temperatura dentro da faixa ideal ou menor que 25°C
+            velocidade = 0;     // Temperatura dentro da faixa ideal (24°C a 22°C), ventilador desligado
         }
-        return true;
+        return true; // O retorno true indica que a operação foi bem-sucedida, mesmo que, no contexto atual, essa função sempre retorna true.
     }
 
     // Função implementada da classe base Atuador que define o valor do ventilador
     bool setValor(int temperatura) {
-        setVelocidade(temperatura);  // Define a velocidade com base na temperatura
+        setVelocidade(temperatura);  // Chama a função "setVelocidade" com base na temperatura
         valor = velocidade;          // Atribui a velocidade ao valor do atuador
-        return true;
+        return true;    // O retorno true indica que a operação foi bem-sucedida, mesmo que, no contexto atual, essa função sempre retorna true.
     }
 };
 
@@ -58,30 +59,30 @@ private:
     int umidificacao;   // Variável para armazenar o valor de umidificação
 
 public:
-    // Construtor do umidificador inicializando o nome do atuador
-    Umidificador() : Atuador("Umidificador") {}
+   // Construtor do umidificador inicializando o nome do atuador e a umidificação com 0
+    Umidificador() : Atuador("Umidificador"), umidificacao(0) {}
 
     // Função para definir a configuração de umidificação com base na umidade
     bool setConfiguracao(int umidade) { // Umidade ideal é 60%
         if (umidade <= 80 && umidade >= 60) {
-            umidificacao = 0;   // Umidade ideal ou mais alta
+            umidificacao = 0;   // Umidade alta, sem a necessidade de utilizar o umidificador
         } else if (umidade < 60 && umidade >= 55) {
-            umidificacao = 5;   // Umidade um pouco abaixo da ideal
+            umidificacao = 5;   // Umidade um pouco abaixo da ideal, pouca utilização do umidificador
         } else if (umidade < 55 && umidade >= 50) {
-            umidificacao = 10;  // Umidade mais baixa
+            umidificacao = 10;  // Umidade um pouco mais longe do ideal, maior utilização do umidificador
         } else if (umidade < 50 && umidade >= 45) {
-            umidificacao = 20;  // Umidade ainda mais baixa
+            umidificacao = 20;  // Umidade longe do ideal, grande utilização do umidificador
         } else {
-            umidificacao = 30;  // Umidade muito baixa
+            umidificacao = 30;  // Umidade muito baixa, utilização máxima do umidificador
         }
-        return true;
+        return true;    // O retorno true indica que a operação foi bem-sucedida, mesmo que, no contexto atual, essa função sempre retorna true.
     }
 
     // Função implementada da classe base Atuador que define o valor do umidificador
     bool setValor(int umidade) {
-        setConfiguracao(umidade);  // Define a configuração com base na umidade
+        setConfiguracao(umidade);  // Chama a função "setConfiguração" com base na umidade
         valor = umidificacao;      // Atribui a umidificação ao valor do atuador
-        return true;
+        return true;    // O retorno true indica que a operação foi bem-sucedida, mesmo que, no contexto atual, essa função sempre retorna true.
     }
 };
 
@@ -91,39 +92,39 @@ private:
     int desumidificacao;   // Variável para armazenar o valor de desumidificação
 
 public:
-    // Construtor do desumidificador inicializando o nome do atuador
-    Desumidificador() : Atuador("Desumidificador") {}
+    // Construtor do desumidificador inicializando o nome do atuador e a desumidificação com 0
+    Desumidificador() : Atuador("Desumidificador"), desumidificacao(0) {}
 
     // Função para definir a configuração de desumidificação com base na umidade
     bool setConfiguracao(int umidade) {  // Umidade ideal é 60%
         if (umidade <= 80 && umidade >= 75) {
             desumidificacao = 30;  // Umidade muito alta, desumidificação máxima
         } else if (umidade < 75 && umidade >= 70) {
-            desumidificacao = 20;  // Umidade alta
+            desumidificacao = 20;  // Umidade alta, desumidificação alta
         } else if (umidade < 70 && umidade >= 65) {
-            desumidificacao = 10;  // Umidade moderada
+            desumidificacao = 10;  // Umidade moderada, desumidificação moderada
         } else if (umidade < 65 && umidade > 60) {
-            desumidificacao = 5;   // Umidade um pouco mais baixa
+            desumidificacao = 5;   // Umidade perto do ideal, desumificação baixa
         } else {
-            desumidificacao = 0;   // Umidade dentro da faixa ideal ou muito baixa
+            desumidificacao = 0;   // Umidade dentro da faixa ideal ou muito baixa, desnecessário a desumidificação
         }
-        return true;
+        return true;    // O retorno true indica que a operação foi bem-sucedida, mesmo que, no contexto atual, essa função sempre retorna true.
     }
 
     // Função implementada da classe base Atuador que define o valor do desumidificador
     bool setValor(int umidade) {
-        setConfiguracao(umidade);  // Define a configuração com base na umidade
+        setConfiguracao(umidade);  // Chama a função "setConfiguração" com base na umidade
         valor = desumidificacao;   // Atribui a desumidificação ao valor do atuador
-        return true;
+        return true;    // O retorno true indica que a operação foi bem-sucedida, mesmo que, no contexto atual, essa função sempre retorna true.
     }
 };
 
 // Classe Lampada que herda de Atuador
 class Lampada : public Atuador {
     private:
-    Luminosidade* sensorLuminosidade;
+    Luminosidade* sensorLuminosidade; // Armazena um ponteiro para um objeto da classe Luminosidade, responsável por medir o brilho ambiente.
 public:
-    // Construtor da lâmpada que recebe um limiar de claridade
+    // Construtor da Lâmpada inicializando o nome do atuador, recebe um sensor de luminosidade como parâmetro e armazena o ponteiro do sensor de luminosidade na variável sensorLuminosidade.
     Lampada(Luminosidade* sensor) : Atuador("Lampada"), sensorLuminosidade(sensor) {}
 
     // Função implementada da classe base Atuador que define o valor da lâmpada
@@ -134,9 +135,10 @@ public:
         } else {
             valor = 1000;  // Lâmpada ligada em brilho máximo
         }
-        return true;
+        return true;    // O retorno true indica que a operação foi bem-sucedida, mesmo que, no contexto atual, essa função sempre retorna true.
+
     }
 
 };
 
-#endif
+#endif  // Finaliza o verificação 
